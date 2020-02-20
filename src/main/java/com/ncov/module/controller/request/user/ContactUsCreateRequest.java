a5fd0ec1fb130251ad7587bf384779f29f803d5a
hbh3819 <hbh3819@163.com>
2020-02-20 20:51:23 +0800

    增加联系我们列表

---
+++ src/main/java/com/ncov/module/controller/request/user/ContactUsCreateRequest.java
@@ -0,0 +1,27 @@
+package com.ncov.module.controller.request.user;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Validated
+public class ContactUsCreateRequest {
+
+    @ApiModelProperty(value = "联系电话")
+    @NotEmpty(message = "联系电话不能为空")
+    private String phone;
+
+    @ApiModelProperty(value = "联系内容")
+    @NotEmpty(message = "联系内容不能为空")
+    private String content;
+
+}
