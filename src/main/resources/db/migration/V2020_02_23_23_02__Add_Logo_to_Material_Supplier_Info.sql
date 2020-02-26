f3dc557ef8721d39d4e6b13f747ffc46f8fd7913
Ran <lrcry@vip.qq.com>
2020-02-26 08:59:25 +0800

    Remove schema reference from flyway script.

--- src/main/resources/db/migration/V2020_02_23_23_02__Add_Logo_to_Material_Supplier_Info.sql
+++ src/main/resources/db/migration/V2020_02_23_23_02__Add_Logo_to_Material_Supplier_Info.sql
@@ -1,5 +1,5 @@
-ALTER TABLE `4ncov`.`material_supplier_info`
+ALTER TABLE `material_supplier_info`
 ADD COLUMN `logo` text NULL COMMENT '供应商logo' AFTER `material_supplier_verify_image_urls`;
 
-ALTER TABLE `4ncov`.`hospital_info`
+ALTER TABLE `hospital_info`
 ADD COLUMN `logo` text NULL AFTER `hospital_contactor_telephone`;
